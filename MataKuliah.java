a3ceec990e6a8d5a63d45e590ca116194c75625b
JHG-007 <jonathan100205@gmail.com>
2024-10-22 20:56:43 +0700

    MK

--- MataKuliah.java
+++ MataKuliah.java
@@ -1,3 +1,25 @@
 public class MataKuliah {
+    private String kodeMK;
+    private int SKS;
+    private String Nama;
+    public MataKuliah(String kodeMK, int sKS, String nama) {
+        this.kodeMK = kodeMK;
+        SKS = sKS;
+        Nama = nama;
+    }
+    public String getKodeMK() {
+        return kodeMK;
+    }
+    public int getSKS() {
+        return SKS;
+    }
+    public String getNama() {
+        return Nama;
+    }
+    @Override
+    public String toString() {
+        return "MataKuliah [kodeMK=" + kodeMK + ", SKS=" + SKS + ", Nama=" + Nama + "]";
+    }
+
     
 }
